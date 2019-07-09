524ed7d61856f02ac6f0d286c7ecfd095ea608d4
architector1324 <olegsajaxov@yandex.ru>
2019-07-10 02:38:52 +0300

    matrix boilerplate

--- deepcl.h
+++ deepcl.h
@@ -1,1 +1,28 @@
 #pragma once
+
+#include <stddef.h>
+
+typedef struct dcl_matf{
+    size_t h, w;
+    float* data;
+} dcl_matf;
+
+typedef struct dcl_matd{
+    size_t h, w;
+    double* data;
+} dcl_matd;
+
+//////////////////////////////////
+//            MATRIX
+//////////////////////////////////
+typedef struct dcl_matf_operations{
+    void(*map)(dcl_matf* A, float(*f)(float),dcl_matf* result);
+    void(*add)(dcl_matf* A, dcl_matf* B, dcl_matf* result);
+    void(*sub)(dcl_matf* A, dcl_matf* B, dcl_matf* result);
+    void(*mul)(dcl_matf* A, dcl_matf* B, dcl_matf* result);
+    void(*had)(dcl_matf* A, dcl_matf* B, dcl_matf* result);
+} dcl_matf_operations;
+
+//////////////////////////////////
+//             DEEP
+//////////////////////////////////
--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include "deepcl.h"
 
 int main(){
+    float _A[] = {1.0f, 2.0f, 3.0f, 4.0f};
+
+    dcl_matf A = {
+        .h = 2,
+        .w = 2,
+        .data = _A
+    };
 
     return 0;
 }
